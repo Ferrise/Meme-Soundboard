5c238158c7d2f8bf9f57210cafb1d6a658b3b725
Ferris Esposito <ferrisesposito@gmail.com>
2019-02-16 17:00:43 -0500

    Add boilerplate for SoundBoard and SoundButton components

---
+++ front-end/src/SoundBoard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// Represents the sound board that the user will interact with
+class SoundBoard extends Component {
+    render() {
+        return(<div>
+               </div>
+            )
+    }
+}
---
+++ front-end/src/SoundButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// Represents a button in the sound board
+class SoundBoard extends Component {
+    render() {
+        return(<button>
+               </button>
+            )
+    }
+}
