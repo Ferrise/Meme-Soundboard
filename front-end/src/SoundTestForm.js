c9fd16bb20c7d9e9380f56e236113f1bd0f990a3
Ferris Esposito <ferrisesposito@gmail.com>
2019-02-16 16:53:33 -0500

    Rename SoundTest

--- front-end/src/SoundTestForm.js
+++ front-end/src/SoundTestForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 // Represents the page where a user can test a sound board
-class SoundTest extends Component {
+class SoundTestForm extends Component {
     render() {
         return(<body>
                </body>
