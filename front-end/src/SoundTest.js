2e38633177dbb608d9af60bc7d266268de7903fc
Ferris Esposito <ferrisesposito@gmail.com>
2019-02-16 16:45:03 -0500

    Create boilerplate for SoundTest class

---
+++ front-end/src/SoundTest.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// Represents the page where a user can test a sound board
+class SoundTest extends Component {
+    render() {
+        return(<body>
+               </body>
+            )
+    }
+}
